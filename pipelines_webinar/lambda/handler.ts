8cf6986e107511ee631599e0fee3a9ee8eb6336f
Israel Bernabe <israel.bernabe@alegra.com>
2021-04-09 09:18:20 -0400

    Modificando respuesta

--- pipelines_webinar/lambda/handler.ts
+++ pipelines_webinar/lambda/handler.ts
@@ -3,6 +3,6 @@ const AWS = require('aws-sdk');
 export const handler = async (event: any = {}): Promise<any> => {
     return {
         body: 'Broken',
-        statusCode: 200,
+        statusCode: 500,
     };
 };
